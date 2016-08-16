833177d791d7fd8d251a023aa6a019ada05472a3
Hans-Peter Herzog <h-p.herzog@web.de>
2016-08-16 11:53:47 +0200

    Added installation instructions

---
+++ coturn-install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+COTURN_VERSION="4.5.0.3"
+COTURN_DIR="coturn-$COTURN_VERSION"
+COTURN_ARCHIVE="$COTURN_DIR.tar.gz"
+COTURN_URL="https://github.com/coturn/coturn/archive/$COTURN_VERSION.tar.gz"
+
+cd /tmp
+wget -O "$COTURN_ARCHIVE" "$COTURN_URL"
+tar xzvf "$COTURN_ARCHIVE"
+cd "$COTURN_DIR"
+./configure
+make && make install
