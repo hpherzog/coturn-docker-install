da9eccd6e88d7805d8dc947e8e4fc53ec9714cf1
Hans-Peter Herzog <h-p.herzog@web.de>
2016-08-16 15:26:10 +0200

    Added installation instructions

--- coturn-install.sh
+++ coturn-install.sh
@@ -5,6 +5,17 @@ COTURN_DIR="coturn-$COTURN_VERSION"
 COTURN_ARCHIVE="$COTURN_DIR.tar.gz"
 COTURN_URL="https://github.com/coturn/coturn/archive/$COTURN_VERSION.tar.gz"
 
+apt-get update && apt-get install -y \
+libssl-dev \
+libevent-dev \
+libsqlite3-dev \
+libpq-dev \
+libmysqlclient-dev \
+libhiredis-dev \
+redis-server \
+gcc \
+make
+
 cd /tmp
 wget -O "$COTURN_ARCHIVE" "$COTURN_URL"
 tar xzvf "$COTURN_ARCHIVE"
